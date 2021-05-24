01bf04b5254cfa570da30ab078af91c5d3201159
unknown <oleksandr.aleksyk@gmail.com>
2021-05-24 23:45:45 +0300

    scroller ready:

--- js/index.js
+++ js/index.js
@@ -14,41 +14,44 @@ window.addEventListener("scroll", function(){
 }, false);
 
 function UpScrollMove(scrollPosition,deltaHeight){
-    if(scrollPosition<deltaHeight && scrollPosition>0){
+    if(scrollPosition<deltaHeight-85 && scrollPosition>0){
         window.scrollTo(0,0);
         AddClassToFlowArea("position-one");
-        console.log("move to section one")
+        SetScroller(1);
     }
-    if(scrollPosition<deltaHeight*2 && scrollPosition>deltaHeight){
+    if(scrollPosition<deltaHeight*2-85 && scrollPosition>deltaHeight){
         window.scrollTo(0,deltaHeight); 
         AddClassToFlowArea("position-two");
-        console.log("move to section two"); 
+        SetScroller(2);
     }
-    if(scrollPosition<deltaHeight*3 && scrollPosition>deltaHeight*2){
+    if(scrollPosition<deltaHeight*3-85 && scrollPosition>deltaHeight*2){
         window.scrollTo(0,deltaHeight*2); 
         AddClassToFlowArea("position-three");
-        console.log("move to section three"); 
+        SetScroller(3);
     }
-    if(scrollPosition<deltaHeight*4 && scrollPosition>deltaHeight*3){
+    if(scrollPosition<deltaHeight*4-85 && scrollPosition>deltaHeight*3){
         window.scrollTo(0,deltaHeight*3); 
         AddClassToFlowArea("position-four");
-        console.log("move to section four"); 
+        SetScroller(4);
     }
 }
 
 
 function DownScrollMove(scrollPosition,deltaHeight) { 
-    if (scrollPosition <= deltaHeight && scrollPosition > 100) {
+    if (scrollPosition <= deltaHeight && scrollPosition > 85) {
         window.scrollTo(0, deltaHeight);
         AddClassToFlowArea("position-two");
+        SetScroller(2);
     }
-    if (scrollPosition <= deltaHeight * 2 && scrollPosition > deltaHeight + 100) {
+    if (scrollPosition <= deltaHeight * 2 && scrollPosition > deltaHeight + 85) {
         window.scrollTo(0, deltaHeight * 2);
         AddClassToFlowArea("position-three");
+        SetScroller(3);
     }
-    if (scrollPosition <= deltaHeight * 3 && scrollPosition > deltaHeight * 2 + 100) {
+    if (scrollPosition <= deltaHeight * 3 && scrollPosition > deltaHeight * 2 + 85) {
         window.scrollTo(0, deltaHeight * 3);
         AddClassToFlowArea("position-four");
+        SetScroller(4);
     }
 }
 
@@ -56,5 +59,32 @@ function AddClassToFlowArea(className){
     flowArea.classList.toggle(flowArea.classList[0]);
     flowArea.classList.add(className); 
 }
-
+function SetScroller(position) {
+    switch (position) {
+        case 1:
+            document.getElementById("scr-one").classList.add("selected");
+            document.getElementById("scr-two").classList.remove("selected");
+            document.getElementById("scr-three").classList.remove("selected");
+            document.getElementById("scr-four").classList.remove("selected");
+            break;
+        case 2:
+            document.getElementById("scr-one").classList.remove("selected");
+            document.getElementById("scr-two").classList.add("selected");
+            document.getElementById("scr-three").classList.remove("selected");
+            document.getElementById("scr-four").classList.remove("selected");
+            break;
+        case 3:
+            document.getElementById("scr-one").classList.remove("selected");
+            document.getElementById("scr-two").classList.remove("selected");
+            document.getElementById("scr-three").classList.add("selected");
+            document.getElementById("scr-four").classList.remove("selected");
+            break;
+        case 4:
+            document.getElementById("scr-one").classList.remove("selected");
+            document.getElementById("scr-two").classList.remove("selected");
+            document.getElementById("scr-three").classList.remove("selected");
+            document.getElementById("scr-four").classList.add("selected");
+            break;
+    }
+}
 
